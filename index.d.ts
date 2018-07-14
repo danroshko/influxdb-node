c0140ec0eba455ea0677130b0301339918332895
Danil Roshko <danroshko@gmail.com>
2018-07-14 10:42:25 +0500

    add types

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+export = Influx
+
+declare class Influx {
+  constructor(options: IOptions = {})
+
+  write(data: string): void
+  writeImmediate(data: string): Promise<any>
+  execute(query: string): Promise<any>
+  query(query: string, convert: boolean = true): Promise<any>
+  formatQueryResponse(response: any, convert: boolean = true): any
+  onError(err: Error): void
+}
+
+interface IOptions {
+  db?: string
+  rp?: string
+  host?: string
+  port?: number
+  maxBufferSize?: number
+  maxBufferTime?: number
+  retries?: number
+  retriesInterval?: number
+}
